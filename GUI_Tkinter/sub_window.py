032d52706924219ac7e23cd40c9ce020a279c124
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-28 15:29:28 +0900

    add code

--- GUI_Tkinter/sub_window.py
+++ GUI_Tkinter/sub_window.py
@@ -7,4 +7,16 @@ root = Tk()
 root.option_add("*font", ("FixedSys", 14))
 root.title("sub window")
 
+def message_window():
+    sub_win = Toplevel()
+    Message(sub_win, text="message のサンプルプログラムです").pack()
+
+# menuの設定
+m = Menu(root)
+root.configure(menu=m)
+m.add_command(label="About", under=0, command=message_window)
+
+# ラベルの設定
+Label(root, text="メニュー About を選択してね").pack()
+
 root.mainloop()
