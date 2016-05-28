35d25c4640776469e84c9d143ba43a2f01b5b814
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-28 10:55:33 +0900

    Add window name & size

--- GUI_Tkinter/button.py
+++ GUI_Tkinter/button.py
@@ -4,6 +4,8 @@
 from Tkinter import *
 
 root = Tk()
+root.title(u"ウインドウ名")
+root.geometry("400x300")
 button = Button(root, text="Python/Tkinter", command=lambda:sys.exit(1))    # ボタンのオブジェクト生成
 label = Label(root, text="Python/label")
 rb = Radiobutton(root, text="python")
