2f2a4002e2cbf4c796d2100551fb193497740748
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-28 13:59:20 +0900

    Revert "Revert "delete the selected item""
    This reverts commit d53acd55494536de720d96ee9a1febc15bb35985.

--- GUI_Tkinter/calc_test.py
+++ GUI_Tkinter/calc_test.py
@@ -5,23 +5,50 @@ from Tkinter import *
 
 root = Tk()
 root.option_add("*font", ("FixedSys", 14))
+root.title(u"式計算")
 
 # 式を格納するオブジェクト
 buff = StringVar()
 buff.set("")
 
+# Entryの生成
+e = Entry(root, textvariable=buff)
+
+# ListBoxの生成
+lb = Listbox(root)
+
+# Scrollbarの生成
+sb1 = Scrollbar(root, orient="v", command=lb.yview)
+sb2 = Scrollbar(root, orient="h", command=lb.xview)
+
+# ListBoxの設定
+lb.configure(yscrollcommand=sb1.set)    # リストボックスのY軸にスクロールバーを設置
+lb.configure(xscrollcommand=sb2.set)    # X軸に
+
+# Gridによる配置
+e.grid(row=0, columnspan=2, sticky="ew")
+lb.grid(row=1, column=0, sticky="nsew")
+sb1.grid(row=1, column=1, sticky="ns")
+sb2.grid(row=2, column=0, sticky="ew")
+
 # 計算
 def calc(event):
-    if buff.get():
-        value = eval(buff.get())    # 文字列の式評価(計算)
-        buff.set(str(value))
+    expr = buff.get()
+    lb.insert("end", expr)
+    lb.see("end")
+    val = eval(expr)
+    buff.set(str(val))
 
-# エントリー
-e = Entry(root, textvariable=buff)
-e.pack()
-e.focus_set()   # フォーカスをエントリー(一行入力ボックス)に
-
+# 取り出し
+def get_expr(event):
+    buff.set(lb.get("active"))
+# 削除
+def delete_expr(event):
+    lb.delete("active")
+    
 # バインディング
 e.bind("<Return>", calc)    # リターンキーが押されたらバインド
+lb.bind("<Double-1>", get_expr) # ダブルクリックで取り出し
+lb.bind("<Delete>", delete_expr)    # デリートキーで削除
 
 root.mainloop()
