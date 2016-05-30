73fec493a6ab7c0091f2793a3bd0a784a5c01199
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-30 14:41:55 +0900

    menu bar

--- GUI_Tkinter/activ.py
+++ GUI_Tkinter/activ.py
@@ -9,15 +9,24 @@ root.option_add("*font", ("FixedSys", 14))
 var = StringVar()
 var.set("nomal")
 
-# button
-b = Button(root, text="button", activeforeground="green", disabledforeground="red")
-b.pack(fill=X)
+def dummy(): pass
+
+# menu
+m0 = Menu(root)
+root.configure(menu=m0)
+
+m1 = Menu(m0, tearoff=False)
+m0.add_cascade(label="Menu", under=0, menu=m1)
+m1.add_command(label="Menu1", command=dummy())
+m1.add_command(label="Menu2", command=dummy())
+m1.add_command(label="Menu3", command=dummy())
 
 # change state
-def change_state(): b.configure(state=var.get())
+def change_state():
+    m1.entryconfigure("Menu1", state=var.get())
 
 # radiobutton settings
-for x in ("nomal", "active", "disabled"):
+for x in ("normal", "active", "disabled"):
     Radiobutton(root, text=x, value=x, variable=var, command=change_state).pack(anchor=W)
 
 root.mainloop()
