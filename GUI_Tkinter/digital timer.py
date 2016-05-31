f144468d987507f108fc1f6dfef0d2583a4b3ea7
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-31 10:26:56 +0900

    Add font menu bar

--- GUI_Tkinter/digital timer.py
+++ GUI_Tkinter/digital timer.py
@@ -6,11 +6,32 @@ from time import *
 
 root = Tk()
 root.option_add("*font", ("FixedSys", 14))
+root.title("Timer")
 
 buff = StringVar()
 buff.set("")
+action = IntVar()
+action.set(0)
 
-Label(textvariable=buff).pack()
+lb = Label(textvariable=buff)
+lb.pack()
+
+def changefont():
+    if action.get() == 0:
+        lb.configure(font=("FixedSys", 20))
+    elif action.get() == 1:
+        lb.configure(font=("Arial", 20))
+    elif action.get() == 2:
+        lb.configure(font=("Times", 20))
+
+mbar = Menu(root)
+root.configure(menu=mbar)
+
+font = Menu(mbar, tearoff=False)
+mbar.add_cascade(label="Font", underline=0, menu=font)
+font.add_radiobutton(label="FixedSys", variable=action, value=0, command=changefont)
+font.add_radiobutton(label="Arial", variable=action, value=1, command=changefont)
+font.add_radiobutton(label="Timer", variable=action, value=2, command=changefont)
 
 def show_time():
     buff.set(strftime("%H:%M:%S"))
