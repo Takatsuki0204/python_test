f24b8b586ef6aa0dfee112b49fef908641ac1f58
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-05-31 11:08:49 +0900

    bug fix

--- GUI_Tkinter/digital timer.py
+++ GUI_Tkinter/digital timer.py
@@ -10,35 +10,38 @@ root.title("Timer")
 
 buff = StringVar()
 buff.set("")
-action = IntVar()
-action.set(0)
+fn = IntVar()
+fn.set(0)
+fs = IntVar()
+fs.set(0)
+
+def changefont():
+    if fn.get() == 0:
+        lb.configure(font=("FixedSys", 20))
+    elif fn.get() == 1:
+        lb.configure(font=("Arial", 50))
+    elif fn.get() == 2:
+        lb.configure(font=("Times", 50))
+
+def show_time():
+    buff.set(strftime("%H:%M:%S"))
+    root.after(1000, show_time)
 
 lb = Label(textvariable=buff)
 lb.pack()
-
-def changefont():
-    if action.get() == 0:
-        lb.configure(font=("FixedSys", 20))
-    elif action.get() == 1:
-        lb.configure(font=("Arial", 20))
-    elif action.get() == 2:
-        lb.configure(font=("Times", 20))
 
 mbar = Menu(root)
 root.configure(menu=mbar)
 
 font = Menu(mbar, tearoff=False)
 mbar.add_cascade(label="Font", underline=0, menu=font)
-font.add_radiobutton(label="FixedSys", variable=action,
-                     value=0, command=changefont)
-font.add_radiobutton(label="Arial", variable=action,
-                     value=1, command=changefont)
-font.add_radiobutton(label="Timer", variable=action,
-                     value=2, command=changefont)
 
-def show_time():
-    buff.set(strftime("%H:%M:%S"))
-    root.after(1000, show_time)     # after:指定された時間(ms)だけ待ち、指定したコマンドを実行(タイマー機能)
+font.add_radiobutton(label="FixedSys", variable=fn,
+                     value=0, command=changefont)
+font.add_radiobutton(label="Arial", variable=fn,
+                     value=1, command=changefont)
+font.add_radiobutton(label="Timer", variable=fn,
+                     value=2, command=changefont)
 
 show_time()
 root.mainloop()
