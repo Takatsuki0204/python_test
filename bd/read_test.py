b1baccc128f48465930bb923f8b5fc81b98ed0a5
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-01 16:50:32 +0900

    add

--- bd/read_test.py
+++ bd/read_test.py
@@ -6,7 +6,7 @@ import sqlite3
 if __name__ == "__main__":
     connector = sqlite3.connect("C:\Users\TAKATSUKI\Documents\環境BOX\sqlite_db\environment.db")
     cursor = connector.cursor()
-    cursor.execute("select * from test")
+    cursor.execute("select * from test_2")
     
     result = cursor.fetchall()
     
--- bd/write_test.py
+++ bd/write_test.py
@@ -6,11 +6,14 @@ import sqlite3
 if __name__ == "__main__":
     connector = sqlite3.connect("C:\Users\TAKATSUKI\Documents\環境BOX\sqlite_db\environment.db")
     
-    sql = "insert into test values('1', 'python')"
+    sql = "create table if not exists test_2(id, name)"
     connector.execute(sql)
-    sql = "insert into test values('2', 'パイソン')"
+    
+    sql = "insert into test_2 values('1', 'python')"
     connector.execute(sql)
-    sql = "insert into test values('3', 'ぱいそん')"
+    sql = "insert into test_2 values('2', 'パイソン')"
+    connector.execute(sql)
+    sql = "insert into test_2 values('3', 'ぱいそん')"
     connector.execute(sql)
     
     connector.commit()
