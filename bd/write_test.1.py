7ab8bc713820fd324692320ca9f50797ad69196f
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-02 10:11:04 +0900

    class of

--- bd/write_test.1.py
+++ bd/write_test.1.py
@@ -3,20 +3,27 @@
 
 import sqlite3
 
-db = "C:\Users\TAKATSUKI\Documents\環境BOX\sqlite_db\environment.db"
-connector = sqlite3.connect(db)
+#db = "C:\Users\TAKATSUKI\Documents\環境BOX\sqlite_db\environment.db"
+#connector = sqlite3.connect(db)
 
-def create_table():
-    sql = "CREATE TABLE IF NOT EXISTS test(id, name, value)"
-    connector.execute(sql)
-    connector.commit()
+class db:
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.connector = sqlite3.connect(dbname)
+        
+    def create_table(self):
+        sql = "CREATE TABLE IF NOT EXISTS test(id, name, value)"
+        self.connector.execute(sql)
+        self.connector.commit()
 
-def put_data(dict):
-    with connector:
-        connector.execute("INSERT INTO test VALUES (:id, :name, :value)", dict)
+    def put_data(self, dict):
+        with self.connector:
+            self.connector.execute("INSERT INTO test VALUES (:id, :name, :value)", dict)
 
 if __name__ == "__main__":
-    create_table()
+    dbn = "C:\Users\TAKATSUKI\Documents\環境BOX\sqlite_db\environment.db"
+    database = db(dbn)
+    database.create_table()
     
     dict = {"id": 1, "name": "python", "value": 12345}
-    put_data(dict)
+    database.put_data(dict)
