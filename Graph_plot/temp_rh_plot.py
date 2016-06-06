fb0e52f724eb32564d12318433342eb4129cc4d0
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-06 15:46:39 +0900

    printing text change

--- Graph_plot/temp_rh_plot.py
+++ Graph_plot/temp_rh_plot.py
@@ -23,23 +23,23 @@ fig = plt.figure(figsize=(15, 5))
 
 # ax1:Temp plot settings
 ax1 = fig.add_subplot(1, 2, 1)
-xfmt = mdates.DateFormatter('%H:%M:%S')
+xfmt = mdates.DateFormatter('%H:%M')
 ax1.xaxis.set_major_formatter(xfmt)
 ax1.yaxis.set_major_locator(tick.MultipleLocator(10))
 ax1.plot(xdate, ytemp)
 ax1.set_ylim(0, 40)
-ax1.set_title("Temp")
+ax1.set_title("Temp:2016-06-06")
 ax1.set_xlabel("Time")
 ax1.set_ylabel("Temp")
 
 # ax2:Humidity plot settings
 ax2 = fig.add_subplot(1, 2, 2)
-xfmt = mdates.DateFormatter('%H:%M:%S')
+xfmt = mdates.DateFormatter('%H:%M')
 ax2.xaxis.set_major_formatter(xfmt)
 ax2.yaxis.set_major_locator(tick.MultipleLocator(10))
 ax2.plot(xdate, yhumidity)
 ax2.set_ylim(0, 100)
-ax2.set_title("Humidity")
+ax2.set_title("Humidity:2016-06-06")
 ax2.set_xlabel("Time")
 ax2.set_ylabel("Humidity")
 
