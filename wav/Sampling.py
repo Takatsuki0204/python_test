0a121c9e01fa13d0223f3a1f4967e61bb735e15e
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-06 11:45:50 +0900

    print wave info

--- wav/Sampling.py
+++ wav/Sampling.py
@@ -4,18 +4,28 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
+def printWaveInfo(wf):
+    """WAVEファイルの情報を取得"""
+    print "チャンネル数:", wf.getnchannels()
+    print "サンプリング幅:", wf.getsampwidth()
+    print "サンプリング周波数:", wf.getframerate()
+    print "フレーム数:", wf.getnframes()
+    print "パラメータ:", wf.getparams()
+    print "長さ(秒):", float(wf.getnframes()) / wf.getframerate()
+
 def main():
     wf = wave.open("C:\Users\TAKATSUKI\Downloads\se_amb01.wav", "r")
-    print "Frame rate: ", wf.getframerate()    # サンプリング周波数
-    print "Sample Width: ", wf.getsampwidth()  # サンプリング幅
+    printWaveInfo(wf)
+    plot(wf)
 
-def plot():
-    wf = wave.open("C:\Users\TAKATSUKI\Downloads\se_amb01.wav", "r")
+def plot(wf):
+    # wf = wave.open("C:\Users\TAKATSUKI\Downloads\se_amb01.wav", "r")
     buf = wf.readframes(wf.getnframes())
+    print len(buf)
     data = np.frombuffer(buf, dtype="int16")
     plt.plot(data)
+    # plt.plot(data[0:500])
     plt.show()
 
 if __name__ == "__main__":
     main()
-    plot()
