70a4332a97a421729c429dc03269706f6dfce730
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-09 14:33:30 +0900

    add code

--- wav/fft.py
+++ wav/fft.py
@@ -2,7 +2,16 @@
 # encoding: utf-8
 import wave
 import numpy as np
-import scipy.fftpack
-from pylab import *
+import scipy as sp
+import matplotlib.pyplot as plt
 
-print "1"
+N = 16
+t = np.arange(N)
+x = sp.sin(2 * sp.pi / N * t)
+X = sp.fft(x)
+
+print x
+plt.plot(np.abs(x), "bo-")
+plt.plot(np.real(x), "go-")
+plt.plot(np.imag(x), "ro-")
+plt.show()
