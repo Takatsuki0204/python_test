4bd56672384156e00755168ead52d36f6ea1f96c
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-09 14:15:26 +0900

    bug fix

--- wav/sin.py
+++ wav/sin.py
@@ -6,11 +6,11 @@ import struct
 import numpy as np
 from pylab import *
 
-def createSinwave(A, f0, fs, len):
+def createSinwave(A, f0, fs, leng):
     """振幅A、基本周波数f0、サンプリング周波数fs、長さlen秒の正弦波を作成して返す"""
     data = []
     # [-1.0, 1.0]の少数値が入った波を作成
-    for n in arange(len * fs):  # nはサンプルインデックス
+    for n in arange(leng * fs):  # nはサンプルインデックス
         s = A * np.sin(2 * np.pi * f0 * n / fs)
         # 振幅が大きい時はクリッピング
         if s > 1.0: s = 1.0
@@ -20,8 +20,8 @@ def createSinwave(A, f0, fs, len):
     data = [int(x * 32767.0) for x in data]
     plot(data[0:50]); show()
     # バイナリに変換
-    #data = struct.pack("h" * len(data), *data)  # listに*をつけると引数展開される
-    #return data
+    data = struct.pack("h" * len(data), *data)  # listに*をつけると引数展開される
+    return data
 
 if __name__ == "__main__":
     freqlist = [262, 294, 330, 349, 392, 440, 494, 523] # ドレミファソラシド
