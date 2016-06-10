c8e89849d2851c26d4d04d9d448a0783d399e3ad
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-10 13:52:20 +0900

    bug fix

--- bd/read_test.2.py
+++ bd/read_test.2.py
@@ -1,14 +1,29 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
-import MySQLdb
+import MySQLdb, datetime
+
+class db:
+    def __init__(self, ip, dbn, usern, pw, char="utf8"):
+        self.connector = MySQLdb.connect(host=ip, db=dbn, user=usern, passwd=pw, charset=char)
+        self.cursor = self.connector.cursor(MySQLdb.cursors.DictCursor)
+    
+    def closed(self):
+        self.connector.close()
+    
+    def create_table(self, sql):
+        self.cursor.execute(sql)
+        self.connector.commit()
 
 if __name__ == "__main__":
-    connector = MySQLdb.connect(host="192.168.10.91", db="db_test", user="user1", passwd="Asdeng", charset="utf8")
-    cursor = connector.cursor()
-    cursor.execute("select * from environment")
+    hostip = "192.168.10.91"
+    dbn = "db_test"
+    usern = "user1"
+    pw = "Asdeng"
+    database = db(hostip, dbn, usern, pw)
+    table = "CREATE TABLE IF NOT EXISTS environment(datetime timestamp, temp float, humidity int)"
+    database.create_table(table)
 
-    print cursor.fetchall
-
-    cursor.close()
-    connector.close()
+    now = datetime.datetime.now()
+    sql = "INSERT INTO environment VALUES('%s', '30.12345', '60')" % str(now)
+    database.create_table(sql)
