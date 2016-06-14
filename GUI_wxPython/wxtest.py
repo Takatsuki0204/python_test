6f9013c8f8d3d9bb8e451c7c6518e81d80136704
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-14 15:19:00 +0900

    frame_size_fixed

--- GUI_wxPython/wxtest.py
+++ GUI_wxPython/wxtest.py
@@ -5,7 +5,7 @@ import wx
 
 app = wx.App()  # アプリケーション初期化
 
-frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム")   # フレーム初期化
+frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300))   # フレーム初期化
 frame.Show()    # フレーム表示 
 
 app.MainLoop()
