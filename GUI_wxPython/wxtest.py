6c3d4ac2ca09ec41902d5cf614f4665d6a1b679e
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-14 15:23:17 +0900

    Add statusbar

--- GUI_wxPython/wxtest.py
+++ GUI_wxPython/wxtest.py
@@ -7,6 +7,9 @@ app = wx.App()  # アプリケーション初期化
 
 frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300), pos=(0, 0))   # フレーム初期化
 #frame.Center() # 中央へ表示
+
+frame.CreateStatusBar()
+frame.SetStatusText("wxPython-test")
 frame.Show()    # フレーム表示 
 
 app.MainLoop()
