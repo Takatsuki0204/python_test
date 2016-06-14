2ed0f4fa4913bfbca13319f4e39c63e4406d0c09
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-14 15:20:58 +0900

    Add display coordinate

--- GUI_wxPython/wxtest.py
+++ GUI_wxPython/wxtest.py
@@ -5,7 +5,8 @@ import wx
 
 app = wx.App()  # アプリケーション初期化
 
-frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300))   # フレーム初期化
+frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300), pos=(0, 0))   # フレーム初期化
+#frame.Center() # 中央へ表示
 frame.Show()    # フレーム表示 
 
 app.MainLoop()
