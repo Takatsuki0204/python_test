17e5d94380131bf274da59b6b48cd0719aa31722
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-15 08:34:58 +0900

    Add

---
+++ GUI_wxPython/grid_test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+import wx
+
+app = wx.App()  # アプリケーション初期化
+frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300))  # フレーム初期化
+
+panel = wx.Panel(frame, wx.ID_ANY)  # パネル初期化
+panel.SetBackgroundColour("#AFAFAF")
+
+button_1 = wx.Button(panel, wx.ID_ANY, "1")
+button_2 = wx.Button(panel, wx.ID_ANY, "2")
+button_3 = wx.Button(panel, wx.ID_ANY, "3")
+button_4 = wx.Button(panel, wx.ID_ANY, "4")
+
+layout = wx.GridSizer(2, 2) # GridSizerを2行2列で初期化
+layout.Add(button_1)
+layout.Add(button_2)
+layout.Add(button_3)
+layout.Add(button_4)
+
+panel.SetSizer(layout)  # パネルへGridSizerをセット
+
+frame.Show()
+app.MainLoop()
