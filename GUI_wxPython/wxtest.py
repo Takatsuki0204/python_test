6196524db4b22c86e2483a9f4ddb4b9a74d8460f
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-14 15:27:53 +0900

    Add icon

--- GUI_wxPython/wxtest.py
+++ GUI_wxPython/wxtest.py
@@ -8,8 +8,12 @@ app = wx.App()  # アプリケーション初期化
 frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 300), pos=(0, 0))   # フレーム初期化
 #frame.Center() # 中央へ表示
 
-frame.CreateStatusBar()
-frame.SetStatusText("wxPython-test")
+frame.CreateStatusBar() # ステータスバー表示
+frame.SetStatusText("wxPython-test")    # バーへのテキスト追加
+
+icon = wx.Icon("~\RoBoHoN.ico", wx.BITMAP_TYPE_ICO)
+frame.SetIcon(icon)
+
 frame.Show()    # フレーム表示 
 
 app.MainLoop()
