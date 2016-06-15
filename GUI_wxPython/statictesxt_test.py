6eac5aee1577d86c04cb3e564b763f513ba036dd
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-15 14:10:12 +0900

    bug fix

--- GUI_wxPython/statictesxt_test.py
+++ GUI_wxPython/statictesxt_test.py
@@ -11,10 +11,11 @@ panel.SetBackgroundColour("#AFAFAF")
 
 # パネルへStaticText(VBでいうラベル)の追加
 text_1 = wx.StaticText(panel, wx.ID_ANY, u"テキスト1")
+t = wx.StaticText(panel, wx.ID_ANY, "")  # 1行開ける場合の小技
 text_2 = wx.StaticText(panel, wx.ID_ANY, u"テキスト2")
-text_3 = wx.StaticText(panel, wx.ID_ANY, u"テキスト3")
-text_4 = wx.StaticText(panel, wx.ID_ANY, u"テキスト4")
-text_5 = wx.StaticText(panel, wx.ID_ANY, u"テキスト5")
+text_3 = wx.StaticText(panel, wx.ID_ANY, u"テキスト3", style=wx.TE_LEFT)
+text_4 = wx.StaticText(panel, wx.ID_ANY, u"テキスト4", style=wx.TE_CENTER)
+text_5 = wx.StaticText(panel, wx.ID_ANY, u"テキスト5", style=wx.TE_RIGHT)
 
 text_3.SetLabel("Text3")    # ラベル変更
 
@@ -27,17 +28,18 @@ text_3.SetBackgroundColour("#00FF00")   # フォント背景カラー変更
 
 text_3.SetToolTipString(u"ラベルです。")    # ツールチップ設定
 
-text_4.Disable()    # 無効化
+text_2.Disable()    # 無効化
 # text_3.Enable()     # 有効化
 # text_3.Hide()     # 非表示
 # text_3.Show()     # 表示
 
 layout = wx.BoxSizer(wx.VERTICAL)
-layout.Add(text_1)
-layout.Add(text_2)
-layout.Add(text_3)
-layout.Add(text_4)
-layout.Add(text_5)
+layout.Add(text_1, flag=wx.GROW)
+layout.Add(t, flag=wx.GROW)
+layout.Add(text_2, flag=wx.GROW)
+layout.Add(text_3, flag=wx.GROW)
+layout.Add(text_4, flag=wx.GROW)
+layout.Add(text_5, flag=wx.GROW)
 
 panel.SetSizer(layout)
 
