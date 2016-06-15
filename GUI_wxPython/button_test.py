1f03dd973158ac0fbbf168208a6e0f30d2e76c0d
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-15 12:53:28 +0900

    bug fix

--- GUI_wxPython/button_test.py
+++ GUI_wxPython/button_test.py
@@ -15,14 +15,22 @@ button_3 = wx.Button(panel, wx.ID_ANY, u"ボタン3")
 
 # button_3.SetLabel(u"Button_3")  # ラベル変更
 # button_3.SetSize((50, 50))      # サイズ変更
-#button_3.SetMinSize((50, 50))
-#button_3.SetMaxSize((50, 50))
+# button_3.SetMinSize((50, 50))
+# button_3.SetMaxSize((50, 50))
 
 font = wx.Font(20, wx.FONTFAMILY_DEFAULT, wx.FONTWEIGHT_NORMAL,
                wx.FONTWEIGHT_NORMAL)   # フォント設定
 button_3.SetFont(font)  # フォントセット
 
-button_3.SetForegroundColour("#FF0000") #フォントカラー変更
+button_3.SetForegroundColour("#FF0000") # フォントカラー変更
+button_3.SetBackgroundColour("#00FF00") # ボタンバックカラー変更
+
+button_3.Disable()    # 無効化
+# button_3.Enable()     # 有効化
+# button_3.Hide()     # 非表示
+# button_3.Show()     # 表示
+
+button_1.SetToolTipString(u"ボタンです") # ツールチップ設定
 
 layout = wx.BoxSizer(wx.VERTICAL)
 layout.Add(button_1)
