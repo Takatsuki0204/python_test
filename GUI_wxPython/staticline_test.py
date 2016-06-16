b116ba0f3ec88575ca3b3726a8d4d9750745cc50
Takatsuki0204 <takatsuki0204@asahi-gp.co.jp>
2016-06-17 08:16:01 +0900

    Add code

--- GUI_wxPython/staticline_test.py
+++ GUI_wxPython/staticline_test.py
@@ -3,3 +3,25 @@
 
 import wx
 
+app = wx.App()
+frame = wx.Frame(None, wx.ID_ANY, u"テストフレーム", size=(300, 200))
+
+panel = wx.Panel(frame, wx.ID_ANY)
+panel.SetBackgroundColour("#AFAFAF")
+
+button_1 = wx.Button(panel, wx.ID_ANY, "1")
+button_2 = wx.Button(panel, wx.ID_ANY, "2")
+button_3 = wx.Button(panel, wx.ID_ANY, "3")
+button_4 = wx.Button(panel, wx.ID_ANY, "4")
+
+layout = wx.BoxSizer(wx.VERTICAL)
+layout.Add(button_1)
+layout.Add(button_2)
+layout.Add(wx.StaticLine(panel), flag=wx.GROW)  # ライン
+layout.Add(button_3)
+layout.Add(button_4)
+
+panel.SetSizer(layout)
+
+frame.Show()
+app.MainLoop()
